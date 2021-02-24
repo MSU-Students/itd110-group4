711c075cfee75f7b43e4c92d65b729bc889588a7
Abdul Moiz Solaiman <solaiman.am85@s.msumain.edu.ph>
2021-02-25 01:54:02 +0800

    Create acceptStudent function

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 var level = require('level');
 var db = level('./leveldb');
+
+var status = ['Applying', 'Under Interview', 'Exam Pending', 'Admitted', 'Probationary'];
+
+acceptStudent('201812485', 'Abdul Moiz Solaiman', 22, 'Marawi City');
+
+function acceptStudent(id, fullName, age, address){
+    db.put(id, [id, fullName, age, address], function(err){
+        //At this point 201812485 = ['201812485', 'Abdul Moiz Solaiman', 22, 'Marawi City']
+    })
+    db.get(id, function(err, value){
+        console.log(value, status[0]); 
+    })
+}
