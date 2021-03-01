b439c0208aba443873642f6d53914084e7dc969c
Bashier, Abdulwalie G <71681622+Abdulwalie02@users.noreply.github.com>
2021-03-01 14:47:41 +0800

    Create scheduleExam function

--- main.js
+++ main.js
@@ -26,12 +26,26 @@ async function acceptStudent(id, fullName, age, address){
 }
 
 async function scheduleInterview(id, scheduleDate){
-    return await db.get(id, function(err, value){
+    await db.get(id, function(err, value){
         if(err){
             console.log(err);
         } else{
             console.log(value, status[1]);
             console.log('Interview date on', scheduleDate);
+        }
+    })
+    return await scheduleExam(id, scheduleDate);
+}
+
+async function scheduleExam(id, scheduleDate){
+    var examDate = new Date(scheduleDate);
+    examDate.setDate(examDate.getDate() + 10);
+    return await db.get(id, function(err, value){
+        if(err){
+            console.log(err);
+        } else{
+            console.log(value, status[2]);
+            console.log('Exam date on', examDate);
         }
     })
 }
